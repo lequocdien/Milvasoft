99a39f19963f84f4486e2dbdb0e42c5ecdd697e2
Le Quoc Dien <159578@thegioididong.com>
2024-07-30 15:11:01 +0700

    add: practice demo proj

---
+++ practice/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using Milvasoft.Core.EntityBases.MultiTenancy;
+using Milvasoft.Core.Exceptions;
+using Milvasoft.Core.Helpers;
+using Milvasoft.Core.Helpers.GeoLocation.Models;
+
+namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> lst = new();
+        Console.WriteLine(lst.IsNullOrEmpty());
+    }
+}
